f065cd1a28b927b4f4643062d8037ebc789e0fe3
Anicet Gruet <agruet@paul-f4Ar1s3.clusters.42paris.fr>
2025-01-27 12:05:28 +0100

    free on command error

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 17:28:41 by agruet            #+#    #+#             */
-/*   Updated: 2025/01/21 16:46:53 by agruet           ###   ########.fr       */
+/*   Updated: 2025/01/27 12:01:03 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-pid_t	exec_cmd(char *str, int *pipefd, char **envp)
+pid_t	exec_cmd(char *str, int *pipefd, char **envp, int *fds)
 {
 	char	*cmd;
 	char	**args;
@@ -32,6 +32,8 @@ pid_t	exec_cmd(char *str, int *pipefd, char **envp)
 		dup2(pipefd[1], STDOUT_FILENO);
 		execve(cmd, args, envp);
 		perror("pipex");
+		close_fds(pipefd[0], pipefd[1], fds[0], fds[1]);
+		free_cmd(cmd, args);
 		exit(EXIT_FAILURE);
 	}
 	free_cmd(cmd, args);
@@ -51,7 +53,7 @@ int	main(int ac, char **av, char **envp)
 	pipe(pipefd);
 	i = open_files(ac, av, fds, pipefd);
 	if (!parse_here_doc(av, NULL, ac, pipefd))
-		wait_child(exec_cmd(av[i++], pipefd, envp), &exit_code, 0);
+		wait_child(exec_cmd(av[i++], pipefd, envp, fds), &exit_code, 0);
 	while (i < ac - 1)
 	{
 		dup2(pipefd[0], STDIN_FILENO);
@@ -59,7 +61,7 @@ int	main(int ac, char **av, char **envp)
 		pipe(pipefd);
 		if (i == ac - 2)
 			dup2(fds[1], pipefd[1]);
-		wait_child(exec_cmd(av[i], pipefd, envp), &exit_code, i == ac - 2);
+		wait_child(exec_cmd(av[i], pipefd, envp, fds), &exit_code, i == ac - 2);
 		i++;
 	}
 	close_fds(pipefd[0], pipefd[1], fds[0], fds[1]);
